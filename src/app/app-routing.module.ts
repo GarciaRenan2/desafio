2dd89aece79230e7dc1aaa67cfe3106fa90f65e1
renan.garcia <renan.garcia@connvert.com.br>
2021-08-25 22:29:15 -0300

    Criação da página de exibição das atividades em aberto; Criação da página de exibição das atividades finalizadas; Criação da página adicionar novas atividades;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdicionarComponent } from './atividade/adicionar/adicionar.component';
 
-const routes: Routes = [];
+import { AtividadeComponent } from './atividade/atividade.component';
+import { FinalizadaComponent } from './atividade/finalizada/finalizada.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: AtividadeComponent,
+  },
+  {
+    path: 'finalizadas',
+    component: FinalizadaComponent,
+  },
+  {
+    path: 'adicionar',
+    component: AdicionarComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatButtonModule } from  '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AtividadeComponent } from './atividade/atividade.component';
+import { AdicionarComponent } from './atividade/adicionar/adicionar.component';
+import { ExibirComponent } from './atividade/exibir/exibir.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FinalizadaComponent } from './atividade/finalizada/finalizada.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AtividadeComponent,
+    AdicionarComponent,
+    ExibirComponent,
+    NotFoundComponent,
+    FinalizadaComponent,
+    MessageComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
